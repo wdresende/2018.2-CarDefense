c304b7208949b967debfbf2bc1036778127b1943
João Matheus <joaomatheus.152013@gmail.com>
2018-09-08 00:38:22 -0300

    Create user CRUD
    Co-authored-by: João Matheus de Sousa Rodrigues <joaomatheus.152013@gmail.com>
    Co-authored-by: João Gabriel Antunes <jg.antunes.medeiros@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework import routers
+from cardefense.views import UserViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + router.urls
