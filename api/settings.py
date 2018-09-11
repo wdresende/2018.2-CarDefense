b5d932d5e94ab797999706b52517fe65f77c3076
bielrossi15 <bielrossiborba@gmail.com>
2018-09-10 22:43:07 -0300

    Settings change

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_2!t9@ed$^51+%$bhysipjv1o%8++q!($x2n1bq6*jrd$^9xm8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [0.0.0.0]
+ALLOWED_HOSTS = []
 
 
 # Application definition
